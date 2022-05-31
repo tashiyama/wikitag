14325d0fdbcc90dd73396a11ec3a9d7019060ae1
tashiyama <mototo8201@gmail.com>
2022-05-31 17:50:28 +0900

    wikiの情報を表示

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import wikipedia
 
+# キーワードを設定
+keyword = "python"
+
+# キーワードで検索
 wikipedia.set_lang("ja")
+search_response = wikipedia.search(keyword)
 
-result = wikipedia.page("銀魂")
-
-print(result.summary)
+#検索結果を表示
+print('キーワード:'+ str(keyword) + 'での検索結果は' + str(len(search_response)) + '件です。')
+print(search_response)
+#検索結果のページ内容を表示
+page_data = wikipedia.page(search_response[0])
+print(page_data.content)
--- wikitag.py
+++ wikitag.py
@@ -1,7 +1,14 @@
+from ensurepip import bootstrap
 from flask import Flask
+from flask import render_template, request, redirect, url_for
+from flask import request
+from flask_bootstrap import Bootstrap
+
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('index.html')
